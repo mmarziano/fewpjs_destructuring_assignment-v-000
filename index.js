9f019a758d1298ac6c77365b8fd23b969bbde2c8
Monica Marziano <mmarziano1@gmail.com>
2019-10-19 21:04:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const k_muppet = {
 };
 
 const keys = [name, color, album, job, partner] 
-const {k_muppet} = keys
+const {k_name, k_color, k_album, k_job, k_partner} = keys
 const {song_2, song_4} = k_muppet.k_album.the_muppet_movie
 
 
